cc52d1b31022d45927d46b47c17d9f9c3907964c
Tim <tim.spiele1@freenet.de>
2023-07-24 20:27:04 +0200

    show the first player

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -8,7 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PlayerComponent implements OnInit {
   @Input() name: string | undefined;
-
+  @Input() playerActive: boolean = false;
   constructor() {
 
   }
